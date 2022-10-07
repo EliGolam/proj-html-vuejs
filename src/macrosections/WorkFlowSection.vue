ef233526fc40e5f3019bc0351d29b0f5e50d04dd
EliGolam <elistasibusiness@gmail.com>
2022-10-07 09:29:58 +0200

    created workflowsection

---
+++ src/macrosections/WorkFlowSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="flex container">
+    
+  </section>
+</template>
+
+<script>
+
+
+export default {
+  name: 'WorkFlowSection',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
